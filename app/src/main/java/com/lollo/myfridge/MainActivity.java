45ee7e05a22be0458bca8e0d50a2d5f09c74abed
Lollo <Lollo>
2016-06-20 00:34:17 +0800

    first commit

---
+++ app/src/main/java/com/lollo/myfridge/MainActivity.java
@@ -0,0 +1,40 @@
+package com.lollo.myfridge;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_greeting)
+    TextView mTvGreeting;
+    @BindView(R.id.rv_ingredients)
+    RecyclerView mRvIngredients;
+    @BindView(R.id.tv_empty_prompt)
+    TextView mTvEmptyPrompt;
+    @BindView(R.id.btn_add_item)
+    FloatingActionButton mBtnAddItem;
+    @BindView(R.id.rv_recipes)
+    RecyclerView mRvRecipes;
+
+    private RecipesListAdapter mRecipesListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initViews();
+    }
+
+    private void initViews() {
+        mRvRecipes.setHasFixedSize(true);
+        mRecipesListAdapter = new RecipesListAdapter();
+        mRvRecipes.setAdapter(mRecipesListAdapter);
+    }
+}
